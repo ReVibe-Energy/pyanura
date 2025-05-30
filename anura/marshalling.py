7c014e0fdb6c6b599b4ea57482847fbad72d62ce
Alexander Mihajlovic <a@abxy.se>
2025-05-30 12:08:24 +0200

    avss: New approach to marshalling message models
    dataclasses_cbor is problematic in that customizing
    the encoding/decode logic for a type relies on adding
    new features to the generic conversion routine.
    Increasing complexity.
    
    This commit introduces marshal/unmarshal. Effectively
    serving the same purpose but with built in support for
    overriding the implemention for individual types.
    
    The new functions have generic implementations for
    dataclasses based on dataclass Fields augmented with
    a cbor_key metadata attribute (just like dataclasses_cbor).
    
    The new marshal/unmarshal functions are used in the avss client
    for converting models.

---
+++ anura/marshalling.py
@@ -0,0 +1,72 @@
+import dataclasses
+import logging
+import types
+import typing
+from dataclasses import is_dataclass
+from typing import Any, Type, TypeVar
+
+T = TypeVar("T")
+
+logger = logging.getLogger(__name__)
+
+
+def cbor_field(cbor_key, /, **kwargs):
+    metadata = kwargs.setdefault("metadata", {})
+    metadata["cbor_key"] = cbor_key
+    return dataclasses.field(**kwargs)
+
+
+def marshal(obj):
+    """
+    Convert an object representation of a message or data type to
+    a structure consisting of dicts, lists and primitive types
+
+    """
+    if hasattr(obj, "_marshal"):
+        return obj._marshal()
+    elif is_dataclass(obj):
+        return {
+            field.metadata["cbor_key"]: marshal(getattr(obj, field.name))
+            for field in dataclasses.fields(obj)
+        }
+    elif isinstance(obj, list):
+        return [marshal(v) for v in obj]
+    elif isinstance(obj, dict):
+        return {marshal(k): marshal(v) for k, v in obj.items()}
+    else:
+        return obj
+
+
+def unmarshal(cls: Type[T], struct) -> T:
+    if hasattr(cls, "_unmarshal"):
+        return getattr(cls, "_unmarshal")(struct)
+    elif is_dataclass(cls):
+        field_by_key = {
+            field.metadata["cbor_key"]: field for field in dataclasses.fields(cls)
+        }
+        attributes = [unmarshal(field_by_key[k].type, v) for k, v in struct.items()]
+        return cls(*attributes)
+    elif isinstance(cls, types.UnionType):
+        match typing.get_args(cls):
+            case inner_cls, types.NoneType:
+                return unmarshal(inner_cls, struct)
+            case _:
+                # In principle this could be extended, but `SomeClass | None`
+                # is enough for our use case.
+                raise ValueError(f"Unsupported union type: {cls}")
+    elif isinstance(cls, types.GenericAlias):
+        origin = typing.get_origin(cls)
+        if origin is list:
+            item_cls = typing.get_args(cls)[0]
+            return [unmarshal(item_cls, v) for v in struct]
+        elif origin is dict:
+            key_cls, val_cls = typing.get_args(cls)
+            return {
+                unmarshal(key_cls, k): unmarshal(val_cls, v) for k, v in struct.items()
+            }
+        else:
+            raise ValueError("Unsupported generic type.")
+    else:
+        if not isinstance(struct, cls):
+            raise TypeError(f"{repr(struct)} not decodable as type {cls}")
+        return struct
