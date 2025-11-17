e0445a39f24f58299644f44eb7cf41d552452029
mikael-tornered <mikael.tornered@revibeenergy.com>
2025-11-17 16:23:50 +0100

    avss: models: field mismatch

--- anura/avss/models.py
+++ anura/avss/models.py
@@ -112,8 +112,8 @@ class SnippetReport:
     start_time: int = cbor_field(0)
     sample_rate: float = cbor_field(1)
     range_: int = cbor_field(2)
-    samples: dict[int, bytes] = cbor_field(3)
     is_synced: bool = cbor_field(4)
+    samples: dict[int, bytes] = cbor_field(3)
 
 
 @dataclass
