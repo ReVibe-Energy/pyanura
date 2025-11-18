96522b8d2aa15b34602bbfdf32e9a5134ae8206c
Alexander Mihajlovic <a@abxy.se>
2025-11-18 06:53:09 +0100

    marshalling: Fix unmarshal dependency on field order
    Unmarshal did not correctly map dict keys to
    dataclass fields when calling the initializer.

--- tests/test_marshalling.py
+++ tests/test_marshalling.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 
-from anura.marshalling import unmarshal, cbor_field
+import pytest
+
+from anura.marshalling import cbor_field, unmarshal
 
 
 def test_unmarshal_dataclass_unknown_key():
@@ -10,3 +12,45 @@ def test_unmarshal_dataclass_unknown_key():
 
     # An unknown key (1) is present
     unmarshal(ClassWithAField, {0: 0, 1: 0})
+
+
+def test_unmarshal_field_order_independence():
+    """
+    Test that unmarshal works independent of dataclass field definition order.
+    """
+
+    @dataclass
+    class FooBar:
+        foo: int = cbor_field(0)
+        bar: bool = cbor_field(1)
+
+    @dataclass
+    class BarFoo:
+        bar: bool = cbor_field(1)
+        foo: int = cbor_field(0)
+
+    test_data = {
+        0: 100,
+        1: True,
+    }
+    a = unmarshal(FooBar, test_data)
+    b = unmarshal(BarFoo, test_data)
+
+    assert a.foo == b.foo
+    assert a.bar == b.bar
+
+
+def test_unmarshal_dataclass_requires_dict():
+    """
+    Test that unmarshal of a dataclass requires a dict as input.
+    """
+
+    @dataclass
+    class Foo:
+        pass
+
+    # No error
+    unmarshal(Foo, {})
+
+    with pytest.raises(ValueError):
+        unmarshal(Foo, [])
